6e5a81d5a7999a1e4156db9119740f91c70ba29d
Zil-M <rhwotmd987@gmail.com>
2022-11-16 00:22:01 +0900

    [ADD] singleton patten practice post

---
+++ Java/Object_Oriented_Programming/SourceCode/chapter2/src/ch2_18_singleton/Company.java
@@ -0,0 +1,15 @@
+package ch2_18_singleton;
+
+public class Company {
+	private static Company instance = new Company();
+	
+	private Company() {};
+	
+	public static Company getInstance() {
+		if(instance == null) {
+			instance = new Company();
+		}
+		
+		return instance;
+	}
+}
