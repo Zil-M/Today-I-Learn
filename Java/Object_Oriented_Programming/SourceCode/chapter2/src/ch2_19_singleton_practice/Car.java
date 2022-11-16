264981acc900f6f146bcfed7ff2e14888ec066a5
Zil-M <rhwotmd987@gmail.com>
2022-11-16 18:14:41 +0900

    [ADD] static, singleton 연습문제 코드

---
+++ Java/Object_Oriented_Programming/SourceCode/chapter2/src/ch2_19_singleton_practice/Car.java
@@ -0,0 +1,13 @@
+package ch2_19_singleton_practice;
+
+public class Car {
+	private int carNumber;
+	
+	public Car(int carNumber) {
+		this.carNumber = carNumber;
+	}
+	
+	public int getCarNumber() {
+		return this.carNumber;
+	}
+}
