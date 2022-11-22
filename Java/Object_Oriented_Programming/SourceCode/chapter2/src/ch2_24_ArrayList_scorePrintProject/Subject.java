1e35de4d92956b576e215e860bc900ab2c4cfba7
Zil-M <rhwotmd987@gmail.com>
2022-11-22 22:10:14 +0900

    [ADD] ArrayList를 활용한 성적 산출 CLI 실습코드

---
+++ Java/Object_Oriented_Programming/SourceCode/chapter2/src/ch2_24_ArrayList_scorePrintProject/Subject.java
@@ -0,0 +1,11 @@
+package ch2_24_ArrayList_scorePrintProject;
+
+public class Subject {
+	public String name;
+	public int score;
+	
+	Subject(String name, int score){
+		this.name = name;
+		this.score = score;
+	}
+}
